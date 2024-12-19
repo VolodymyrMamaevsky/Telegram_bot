3ad66ec18137e8074acda6232555400bf4c18aec
Mamaevsky <vova.moder@gmail.com>
2024-12-19 14:32:23 +0200

    refactor

--- app/database/requests.py
+++ app/database/requests.py
@@ -5,8 +5,8 @@ from sqlalchemy import select
 
 async def set_user(tg_id):
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.id == tg_id))
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
 
     if not user:
-        session.add(User(id=tg_id))
+        session.add(User(tg_id=tg_id))
         await session.commit()
